c1a71557e3949b7eede73ea484aa7a63939a37a9
Evan Oliver <evan.oliver87@gmail.com>
2019-03-01 10:31:24 -0500

    -Moved the ground plane on the Tile_BP down so floor was aligned with Z=0 -Created a rudimentary UFunction in the Tile CPP to select a random number of spawn points within a user-inputted range, and spawn a user-inputted actor at those locations -Implemented this method for Box, Canister, Sandbag, and all three Rock BPs inside the Tile BP

--- Source/S05_TestingGrounds/Terrain/Tile.cpp
+++ Source/S05_TestingGrounds/Terrain/Tile.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tile.h"
+#include "Engine/World.h"
+#include "Engine/Classes/Components/SceneComponent.h"
 
 // Sets default values
 ATile::ATile()
@@ -10,16 +12,20 @@ ATile::ATile()
 
 }
 
-void ATile::PlaceActors()
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
 {
 	FVector Min(0, -2000, 0);
 	FVector Max(4000, 2000, 0);
 	FBox Bounds(Min, Max);
 	
-	for (size_t i = 0; i < 20; i++)
+	int NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+
+	for (size_t i = 0; i < NumberToSpawn; i++)
 	{
 		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
-		UE_LOG(LogTemp, Warning, TEXT("SpawnPoint = %s"), *SpawnPoint.ToCompactString());
+		AActor * Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+		Spawned->SetActorLocation(SpawnPoint);
+		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
 	}
 }
 
